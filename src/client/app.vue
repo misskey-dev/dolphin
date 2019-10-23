1c5ec8112f2df855d726b7c06ea38c6f2b045e31
syuilo <Syuilotan@yahoo.co.jp>
2019-10-23 11:12:54 +0900

    wip

--- src/client/app.vue
+++ src/client/app.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="dp-app">
 	<main>
-		<router-view v-hotkey.global="keymap"></router-view>
+		<transition name="fade-in-top" mode="out-in">
+			<router-view v-hotkey.global="keymap"></router-view>
+		</transition>
 		<div class="powerd-by" :style="{ visibility: $store.getters.isSignedIn ? 'hidden' : 'visible' }">
 			<span>Powered by</span>
 			<x-logo class="logo"/>
@@ -9,25 +11,33 @@
 	</main>
 	<transition name="zoom-in-bottom">
 		<nav v-if="navOpen" ref="nav">
-			<router-link :to="`/@${ $store.state.i.username }`"><dp-avatar :user="$store.state.i" class="avatar"/><dp-user-name :user="$store.state.i"/></router-link>
-			<div></div>
-			<router-link to="/"><fa :icon="faHome" fixed-width/>{{ $t('timeline') }}</router-link>
-			<button class="_buttonPlain" @click="list()"><fa :icon="faListUl" fixed-width/>{{ $t('lists') }}</button>
-			<router-link to="/messages"><fa :icon="faEnvelope" fixed-width/>{{ $t('messages') }}<i v-if="$store.state.i.hasUnreadSpecifiedNotes"><fa :icon="faCircle"/></i></router-link>
-			<router-link to="/favorites"><fa :icon="faStar" fixed-width/>{{ $t('favorites') }}</router-link>
-			<div></div>
-			<router-link to="/settings"><fa :icon="faUserCog" fixed-width/>{{ $t('settings') }}</router-link>
-			<router-link to="/instance"><fa :icon="faCog" fixed-width/>{{ $t('instance') }}</router-link>
-			<div></div>
-			<button class="_buttonPlain" @click="search()"><fa :icon="faSearch" fixed-width/>{{ $t('search') }}</button>
+			<template v-if="!showLists">
+				<router-link :to="`/@${ $store.state.i.username }`"><dp-avatar :user="$store.state.i" class="avatar"/><dp-user-name :user="$store.state.i"/></router-link>
+				<div></div>
+				<button class="_buttonPlain" @click="list()"><fa :icon="faListUl" fixed-width/>{{ $t('lists') }}</button>
+				<router-link to="/messages"><fa :icon="faEnvelope" fixed-width/>{{ $t('messages') }}<i v-if="$store.state.i.hasUnreadSpecifiedNotes"><fa :icon="faCircle"/></i></router-link>
+				<router-link to="/favorites"><fa :icon="faStar" fixed-width/>{{ $t('favorites') }}</router-link>
+				<div></div>
+				<router-link to="/settings"><fa :icon="faUserCog" fixed-width/>{{ $t('settings') }}</router-link>
+				<router-link to="/instance"><fa :icon="faCog" fixed-width/>{{ $t('instance') }}</router-link>
+				<div></div>
+				<button class="_buttonPlain" @click="search()"><fa :icon="faSearch" fixed-width/>{{ $t('search') }}</button>
+			</template>
+			<template v-else>
+				<span v-if="lists.length === 0" style="opacity: 0.5; pointer-events: none;">{{ $t('noLists') }}</span>
+				<router-link v-for="list in lists" :to="`/lists/@${ list.id }`" :key="list.id">{{ list.name }}</router-link>
+			</template>
 		</nav>
 	</transition>
-	<transition name="zoom-in-bottom">
+	<transition name="zoom-in-top">
 		<x-notifications v-if="notificationsOpen" class="notifications"/>
 	</transition>
-	<button v-if="$store.getters.isSignedIn" class="button nav _buttonPlain" @click="() => { navOpen = !navOpen; notificationsOpen = false; }" ref="navButton"><fa :icon="navOpen ? faTimes : faBars"/><i v-if="$store.state.i.hasUnreadSpecifiedNotes"><fa :icon="faCircle"/></i></button>
-	<button v-if="$store.getters.isSignedIn" class="button notifications _buttonPlain" @click="notificationsOpen = !notificationsOpen" ref="notificationsButton"><fa :icon="notificationsOpen ? faTimes : faBell"/><i v-if="$store.state.i.hasUnreadNotification"><fa :icon="faCircle"/></i></button>
-	<button v-if="$store.getters.isSignedIn" class="button post _buttonPrimary" @click="post()"><fa :icon="faPencilAlt"/></button>
+	<div class="buttons">
+		<button v-if="$store.getters.isSignedIn" class="button nav _buttonPlain" @click="() => { navOpen = !navOpen; notificationsOpen = false; }" ref="navButton"><fa :icon="navOpen ? faTimes : faBars"/><i v-if="$store.state.i.hasUnreadSpecifiedNotes"><fa :icon="faCircle"/></i></button>
+		<button v-if="$store.getters.isSignedIn" class="button home _buttonPlain" :disabled="$route.path === '/'" @click="$router.push('/')"><fa :icon="faHome"/></button>
+		<button v-if="$store.getters.isSignedIn" class="button notifications _buttonPlain" @click="notificationsOpen = !notificationsOpen" ref="notificationsButton"><fa :icon="notificationsOpen ? faTimes : faBell"/><i v-if="$store.state.i.hasUnreadNotification"><fa :icon="faCircle"/></i></button>
+		<button v-if="$store.getters.isSignedIn" class="button post _buttonPrimary" @click="post()"><fa :icon="faPencilAlt"/></button>
+	</div>
 </div>
 </template>
 
@@ -53,6 +63,8 @@ export default Vue.extend({
 			searching: false,
 			navOpen: false,
 			notificationsOpen: false,
+			showLists: false,
+			lists: [],
 			connection: null,
 			faPencilAlt, faBars, faTimes, faBell, faSearch, faUserCog, faCog, faUser, faHome, faStar, faCircle, faAt, faEnvelope, faListUl
 		};
@@ -74,6 +86,7 @@ export default Vue.extend({
 		},
 
 		navOpen(opened) {
+			this.showLists = false;
 			const onMousedown = e => {
 				e.preventDefault();
 				if (
@@ -92,6 +105,12 @@ export default Vue.extend({
 				}
 			}
 		},
+
+		async showLists() {
+			if (this.showLists) {
+				this.lists = await this.$root.api('users/lists/list');
+			}
+		}
 	},
 
 	created() {
@@ -121,6 +140,10 @@ export default Vue.extend({
 					this.searching = false;
 				});
 			});
+		},
+
+		list() {
+			this.showLists = true;
 		},
 
 		onNotification(notification) {
@@ -250,82 +273,81 @@ export default Vue.extend({
 
 	> .notifications {
 		position: fixed;
-		bottom: 96px + 16px;
+		top: 16px;
 		left: 0;
 		right: 0;
 		margin: 0 auto;
 		z-index: 10001;
 		width: 290px;
 		height: 300px;
-
-		@media (max-width: 500px) {
-			bottom: 92px;
-		}
 	}
 
-	> .button {
+	> .buttons {
 		position: fixed;
 		z-index: 1000;
 		bottom: 32px;
-		padding: 0;
-		width: 64px;
-		height: 64px;
-		border-radius: 100%;
-		box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
-
-		> * {
-			font-size: 24px;
-		}
-
-		&:focus {
-			outline: none;
-		}
-
-		&.nav, &.notifications {
-			background: var(--bg);
-			color: var(--fg);
-
-			&:hover {
-				background: #eee;
-
-				@media (prefers-color-scheme: dark) {
-					background: #1c1e1f;
-				}
-			}
-
-			> i {
-				position: absolute;
-				top: 0;
-				left: 0;
-				color: $primary;
-				font-size: 16px;
-				animation: blink 1s infinite;
-			}
-		}
-
-		&.nav {
-			left: 32px;
-		}
-
-		&.notifications {
-			left: 0;
-			right: 0;
-			margin: 0 auto;
-		}
-
-		&.post {
-			right: 32px;
-		}
+		padding: 0 32px;
+		display: flex;
+		width: 100%;
+		box-sizing: border-box;
 
 		@media (max-width: 500px) {
 			bottom: 16px;
+			padding: 0 16px;
+		}
 
-			&.nav {
-				left: 16px;
+		> .button {
+			padding: 0;
+			margin: auto;
+			width: 64px;
+			height: 64px;
+			border-radius: 100%;
+			box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2), 0 6px 10px 0 rgba(0, 0, 0, 0.14), 0 1px 18px 0 rgba(0, 0, 0, 0.12);
+
+			&:first-child {
+				margin-left: 0;
 			}
 
-			&.post {
-				right: 16px;
+			&:last-child {
+				margin-right: 0;
+			}
+
+			> * {
+				font-size: 24px;
+			}
+
+			&:focus {
+				outline: none;
+			}
+
+			&:disabled {
+				cursor: default;
+
+				> * {
+					opacity: 0.5;
+				}
+			}
+
+			&:not(.post) {
+				background: var(--bg);
+				color: var(--fg);
+
+				&:hover {
+					background: #eee;
+
+					@media (prefers-color-scheme: dark) {
+						background: #1c1e1f;
+					}
+				}
+
+				> i {
+					position: absolute;
+					top: 0;
+					left: 0;
+					color: $primary;
+					font-size: 16px;
+					animation: blink 1s infinite;
+				}
 			}
 		}
 	}
