83cd7f554ba237f574ca67f3198efaa58dda31cd
syuilo <Syuilotan@yahoo.co.jp>
2019-10-28 09:27:18 +0900

    Update federation.vue

--- src/client/pages/federation.vue
+++ src/client/pages/federation.vue
@@ -76,6 +76,16 @@ export default Vue.extend({
 			
 			> .instances {
 				> .instance {
+					&:hover {
+						background: rgba(0, 0, 0, 0.05);
+						box-shadow: 0 0 0 8px rgba(0, 0, 0, 0.05);
+
+						@media (prefers-color-scheme: dark) {
+							background: rgba(255, 255, 255, 0.05);
+							box-shadow: 0 0 0 8px rgba(255, 255, 255, 0.05);
+						}
+					}
+
 					> .host {
 						> .indicator {
 							font-size: 70%;
