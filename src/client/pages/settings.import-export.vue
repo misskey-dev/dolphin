8c6288b4711665993b75f3bba7c80833421bcd25
syuilo <Syuilotan@yahoo.co.jp>
2019-11-03 16:40:21 +0900

    Fix #20

--- src/client/pages/settings.import-export.vue
+++ src/client/pages/settings.import-export.vue
@@ -2,6 +2,7 @@
 <section class="dp-settings-page-import-export _section">
 	<div class="title"><fa :icon="faBoxes"/> {{ $t('importAndExport') }}</div>
 	<div class="content">
+		<input ref="file" type="file" style="display: none;" @change="onChangeFile"/>
 		<x-select v-model="exportTarget" style="margin-top: 0;">
 			<option value="notes">{{ $t('_exportOrImport.allNotes') }}</option>
 			<option value="following">{{ $t('_exportOrImport.followingList') }}</option>
@@ -21,6 +22,7 @@ import { faDownload, faUpload, faBoxes } from '@fortawesome/free-solid-svg-icons
 import XButton from '../components/ui/button.vue';
 import XSelect from '../components/ui/select.vue';
 import i18n from '../i18n';
+import { apiUrl } from '../config';
 
 export default Vue.extend({
 	i18n,
@@ -59,25 +61,61 @@ export default Vue.extend({
 		},
 
 		doImport() {
-			this.$chooseDriveFile().then(file => {
-				this.$root.api(
-					this.exportTarget == 'following' ? 'i/import-following' :
-					this.exportTarget == 'user-lists' ? 'i/import-user-lists' :
-					null, {
-						fileId: file.id
-				}).then(() => {
-					this.$root.dialog({
-						type: 'info',
-						text: this.$t('importRequested')
-					});
-				}).catch((e: any) => {
-					this.$root.dialog({
-						type: 'error',
-						text: e.message
-					});
+			(this.$refs.file as any).click();
+		},
+
+		onChangeFile() {
+			const [file] = Array.from((this.$refs.file as any).files);
+			
+			const data = new FormData();
+			data.append('file', file);
+			data.append('i', this.$store.state.i.token);
+
+			const dialog = this.$root.dialog({
+				type: 'waiting',
+				text: this.$t('uploading') + '...',
+				showOkButton: false,
+				showCancelButton: false,
+				cancelableByBgClick: false
+			});
+
+			fetch(apiUrl + '/drive/files/create', {
+				method: 'POST',
+				body: data
+			})
+			.then(response => response.json())
+			.then(f => {
+				this.reqImport(f);
+			})
+			.catch(e => {
+				this.$root.dialog({
+					type: 'error',
+					text: e
 				});
+			})
+			.finally(() => {
+				dialog.close();
 			});
 		},
+
+		reqImport(file) {
+			this.$root.api(
+				this.exportTarget == 'following' ? 'i/import-following' :
+				this.exportTarget == 'user-lists' ? 'i/import-user-lists' :
+				null, {
+					fileId: file.id
+			}).then(() => {
+				this.$root.dialog({
+					type: 'info',
+					text: this.$t('importRequested')
+				});
+			}).catch((e: any) => {
+				this.$root.dialog({
+					type: 'error',
+					text: e.message
+				});
+			});
+		}
 	}
 });
 </script>
