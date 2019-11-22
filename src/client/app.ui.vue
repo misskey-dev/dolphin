53d9f4d2d019939eb549de5f721cd8cab109bbd6
okpierre <1679025+okpierre@users.noreply.github.com>
2019-11-22 18:49:23 -0500

    Update app.ui.vue (#35)
    * Update app.ui.vue
    
    -match top of notification to the top of timeline
    -move notification to the right for tablet and desktop view 
    -increase height to fit four notifications (default) and remove scroll bar
    -decrease height to three notifications when viewing in mobile landscape view to avoid blocking buttons
    
    * Some tweak

--- src/client/app.ui.vue
+++ src/client/app.ui.vue
@@ -300,13 +300,21 @@ export default Vue.extend({
 
 	> .notifications {
 		position: fixed;
-		top: 16px;
-		left: 0;
-		right: 0;
+		top: 32px;
+		right: 7rem;
 		margin: 0 auto;
 		z-index: 10001;
 		width: 290px;
-		height: 300px;
+		height: 305px;
+		
+		@media (max-width: 849px) {
+			left: 0;
+			right: 0;
+		}
+
+		@media (max-width: 849px) and (orientation: landscape) {
+			height: 230px;
+		}
 	}
 
 	> .buttons {
